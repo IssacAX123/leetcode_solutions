cdbd5a9840aa2a76d58859a121a773482a282317
Issac Abraham <issaca32@gmail.com>
2021-07-22 10:45:33 +0100

    adds 389. Find the difference

---
+++ 389. Find the Difference.py
@@ -0,0 +1,15 @@
+import collections
+def findTheDifference_issac(s: str, t: str) -> str:
+    s_counter = collections.Counter(s)
+    t_counter = collections.Counter(t)
+    for key in t_counter:
+        if key not in s_counter:
+            return key
+        else:
+            if t_counter[key] != s_counter[key]:
+                return key
+
+print(findTheDifference("a", "aa"))
+
+
+
