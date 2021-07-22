acd8fcf48511d44b27f4f7631aee26260af2c79e
Issac Abraham <issaca32@gmail.com>
2021-07-22 11:05:11 +0100

    adds 389. Find the difference v3

--- 389. Find the Difference.py
+++ 389. Find the Difference.py
@@ -1,4 +1,5 @@
 import collections
+#my one is faster
 def findTheDifference_issac(s: str, t: str) -> str:
     s_counter = collections.Counter(s)
     t_counter = collections.Counter(t)
@@ -9,7 +10,26 @@ def findTheDifference_issac(s: str, t: str) -> str:
             if t_counter[key] != s_counter[key]:
                 return key
 
-print(findTheDifference_issac("a", "aa"))
+def findTheDifference_online(s: str, t:str) -> str:
+    i = 0
+    sum = 0
+    while i < len(s):
+        sum += (ord(t[i]) - ord(s[i]))
+        i+= 1
+    sum += ord(t[i])
+    return chr(sum)
+
+# optimal between space and time
+def findTheDifference_online_2(s: str, t:str) -> str:
+    i = 0
+    res = 0
+    while i < len(s):
+        res = res ^ ord(s[i])
+        res = res ^ ord(t[i])
+        i+=1
+    res = res ^ ord(t[i])
+    return chr(res)
+print(findTheDifference_online_2("aaabb", "aaabbc"))
 
 
 
