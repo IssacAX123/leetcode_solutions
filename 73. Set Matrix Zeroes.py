19292dfe234b6087b68c30eb7eb6b6090c1d0f4e
Issac Abraham <issaca32@gmail.com>
2021-08-11 13:14:08 +0100

    updates 73. Set Matrix Zeroes

--- 73. Set Matrix Zeroes.py
+++ 73. Set Matrix Zeroes.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# o(m + n) space
 def setZeroes_issac(matrix: List[List[int]]) -> None:
     """
     Do not return anything, modify matrix in-place instead.
@@ -24,7 +25,41 @@ def setZeroes_issac(matrix: List[List[int]]) -> None:
     for row in matrix:
         print(row)
 
-print(setZeroes_issac(
-[[1,1,1],
- [1,0,1],
- [1,1,1]]))
+
+# o(1) space but not working
+def setZeroes_issac_2(matrix: List[List[int]]) -> None:
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    row1 = 1
+    for i, r in enumerate(matrix):
+        for j, c in enumerate(r):
+            if c == 0:
+                matrix[0][j] = 0
+                if i == 0:
+                    row1 = 0
+                else:
+                    matrix[i][0] = 0
+
+    for x, rowv in enumerate(matrix[0]):
+        if rowv == 0:
+            for y in range(len(matrix)):
+                matrix[y][x] = 0
+
+    if row1 == 0:
+        for x in range(len(matrix[0])):
+            matrix[0][x] = 0
+
+    for i in range(len(matrix)):
+        if matrix[i][0] == 0:
+            for x in range(len(matrix[0])):
+                matrix[i][x] = 0
+
+    for row in matrix:
+        print(row)
+
+
+print(setZeroes_issac_2(
+    [[0, 1, 2, 0],
+     [3, 4, 5, 2],
+     [1, 3, 1, 5]]))
