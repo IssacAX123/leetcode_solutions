a03a667e5076b20802b3677763f084c51d9b4fea
Issac Abraham <issaca32@gmail.com>
2021-08-11 12:42:22 +0100

    adds 73. Set Matrix Zeroes

---
+++ 73. Set Matrix Zeroes.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+def setZeroes_issac(matrix: List[List[int]]) -> None:
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    rows = []
+    columns = []
+    for i, r in enumerate(matrix):
+        for j, c in enumerate(r):
+            if c == 0:
+                if i not in rows:
+                    rows.append(i)
+                if j not in columns:
+                    columns.append(j)
+    for row in rows:
+        for j in range(len(matrix[row])):
+            matrix[row][j] = 0
+
+    for column in columns:
+        for i in range(len(matrix)):
+            matrix[i][column] = 0
+    for row in matrix:
+        print(row)
+
+print(setZeroes(
+[[1,1,1],
+ [1,0,1],
+ [1,1,1]]))
